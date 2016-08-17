c43633166c939eebdaa326ba9c268e6d7ac6e034
tim taylor <timet84@vt.edu>
2016-08-17 21:48:30 +0000

    fix submodule

---
+++ week2/js-report/jsstringreporter.js
@@ -0,0 +1,8 @@
+
+var awesome = prompt("Whats your favorite color?");
+alert(awesome+awesome.length); 
+alert(awesome.charAt(2));
+alert(awesome.toLowerCase())
+alert(awesome.toUpperCase())
+alert(awesome + "Is the shit!")
+alert(awesome.substring(2,4))
---
+++ week2/simple-validator/validator.js
@@ -0,0 +1,44 @@
+//telephone
+function phone(){
+ var cell = prompt("Whats your number","");
+ if(cell.charAt(3) === "-" && cell.charAt(7) === "-" && cell.length === 12){
+ alert("thanks ill call you");
+ console.log("true");
+ birth();}
+else{
+    console.log('try again')
+    alert("thats wrong");
+    phone();
+}
+}
+
+
+function birth(){
+    var birthday = prompt("Whats your birthday","");
+    if(birthday.charAt(3) === "/" && birthday.charAt(6) === "/")
+    zip()
+else{ 
+    console.log("WHAT THATS WRONG!!!!")
+    alert("DO IT OVER!!!")
+    birth()
+    }
+}
+
+function zip(){
+    var zipcode = prompt("tell me where you stay at")
+    if (zipcode.charAt(5)){
+    state()
+    }
+  else{
+        console.log("That aint right!")
+        alert("try again")
+        zip()
+    }
+}  
+function state(){
+    var state = prompt("tell me your state in two letters")
+    if (state.charAt(2) && state.toUpperCase(2)){
+     
+    
+    } 
+}
