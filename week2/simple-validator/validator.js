a6df29445cfde5abc902a235309da2decf7c986a
Tim Taylor <timtaylor@Tims-MacBook-Pro.local>
2016-08-17 16:39:50 -0600

    oter

--- week2/simple-validator/validator.js
+++ week2/simple-validator/validator.js
@@ -1,6 +1,7 @@
 //telephone
 function phone(){
- var cell = prompt("Whats your number","");
+ console.log("working")
+ var cell = prompt("Whats your number");
  if(cell.charAt(3) === "-" && cell.charAt(7) === "-" && cell.length === 12){
  alert("thanks ill call you");
  console.log("true");
@@ -11,13 +12,13 @@ else{
     phone();
 }
 }
-
+// phone()
 
 function birth(){
     var birthday = prompt("Whats your birthday","");
     if(birthday.charAt(3) === "/" && birthday.charAt(6) === "/")
     zip()
-else{ 
+else{
     console.log("WHAT THATS WRONG!!!!")
     alert("DO IT OVER!!!")
     birth()
@@ -34,11 +35,12 @@ function zip(){
         alert("try again")
         zip()
     }
-}  
+}
 function state(){
     var state = prompt("tell me your state in two letters")
     if (state.charAt(2) && state.toUpperCase(2)){
-     
-    
-    } 
+
+
+    }
 }
+phone()
